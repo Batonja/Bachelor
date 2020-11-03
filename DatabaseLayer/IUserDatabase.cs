41ef4dacbdf82d55e7ca2a75a41ca91e87351c63
Nedeljko Zekonja <batonja@hotmail.com>
2020-11-03 15:53:44 +0100

    Add project files.

---
+++ DatabaseLayer/IUserDatabase.cs
@@ -0,0 +1,20 @@
+﻿using Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseLayer
+{
+    public interface IUserDatabase
+    {
+        bool SignUp(User user);
+
+        List<User> GetUsers();
+        List<Friend> GetFriends();
+        Friend ConfirmFriendship(Friend friend);
+        User GetUserByPassportId(long passportId);
+
+        User FindUserOrAdd(User user);
+        bool AddFriend(Friend friend);
+    }
+}
